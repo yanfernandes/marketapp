9f4aa17a6f9a3546a9047249831ff60ba19857af
Yan <yanfernandes@gmail.com>
2021-05-08 12:30:47 -0300

    Create api to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  resources :products, :orders
+  resources :products, :orders, :promotions
+
+  namespace 'api' do
+  	namespace 'v1' do
+      resources :products
+    end
+  end
+
+  namespace 'api' do
+  	namespace 'v1' do
+      resources :orders
+    end
+  end
 
   root to: "home#index"
-
 end
